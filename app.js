282ad73eb200e4356b9815748862408705344987
sreebadhri <arumugaprabu.s@gmail.com>
2019-02-20 22:28:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('OpenShift Home Page!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello OpenShift!');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.send('Hello Mr.' + req.params.name + ' to OpenShift World!');
+});
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
